88bd8da44c7f656b7c80e030588c303dd7001590
EneaWeb <giovannicweb@gmail.com>
2017-02-22 12:19:28 +0100

    fix price

--- app/Order.php
+++ app/Order.php
@@ -8,9 +8,9 @@ class Order extends Model
 
     protected $table = 'orders';
     /**
-     * The attributes that are mass assignable.
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    * @var array
+    */
     protected $fillable = [
         'customer_name', 
         'customer_email',
@@ -20,42 +20,42 @@ class Order extends Model
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
-     * @var array
-     */
+    * The attributes that should be hidden for arrays.
+    * @var array
+    */
     protected $hidden = [
     ];
 
     /**
-     * Relations
-     */
+    * Relations
+    */
 
-     public function customer()
-     {
-         return $this->belongsTo('\App\Customer');
-     }
+    public function customer()
+    {
+        return $this->belongsTo('\App\Customer');
+    }
 
-     public function details()
-     {
-         return $this->hasMany('\App\OrderDetail');
-     }
+    public function details()
+    {
+        return $this->hasMany('\App\OrderDetail');
+    }
 
-     public function supplierTotal()
-     {
-         $a = 0;
-         foreach ($this->details as $detail) {
-             $a += $detail->supplier_price;
-         }
-         return number_format($a, 2, ',', '.');
-     }
+    public function supplierTotal()
+    {
+        $a = 0;
+        foreach ($this->details as $detail) {
+            $a += $detail->supplier_price;
+        }
+        return number_format($a, 2, ',', '.');
+    }
 
-     public function total()
-     {
-         $a = 0;
-         foreach ($this->details as $detail) {
-             $a += $detail->final_price;
-         }
-         return number_format($a, 2, ',', '.');
-     }
+    public function total()
+    {
+        $a = 0;
+        foreach ($this->details as $detail) {
+        $a += $detail->total_price;
+        }
+        return number_format($a, 2, ',', '.');
+    }
 
 }
